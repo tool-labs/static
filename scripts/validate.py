02d8c785b1647730c615f7c6d0bdd20b0279e238
Robin Krahl <me@robin-krahl.de>
2014-10-26 22:02:19 +0100

    Update scripts/validate.py.

--- scripts/validate.py
+++ scripts/validate.py
@@ -1,7 +1,10 @@
 # -*- coding: utf-8 -*-
 
+import logging
+logging.basicConfig(level=logging.INFO)
+
 import static_browser.parser
 
-parser = static_browser.parser.PackageParser('res')
+parser = static_browser.parser.NewPackageParser('res')
 if not parser.validate():
     exit(1)
